255f813a52398bfb135da22dfdea9155da4891a8
Dee0105 <bdeeptimayee966@gmail.com>
2026-03-15 23:15:17 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='customer_support_chatbot',
+    version='0.1.0',
+    author='Deepti',
+    author_email='bdeeptimayee966@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
